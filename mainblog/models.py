32523303137660e75916fa3bf5dd0c75d2ec5583
layersony <layersony@gmail.com>
2021-11-06 15:05:16 -0400

    migrations

--- mainblog/models.py
+++ mainblog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class BaseBlog(models.Model):
   created = models.DateTimeField(auto_now_add=True)
-  updated = models.DateTimeField(auto_created=True)
+  updated = models.DateTimeField(auto_now=True)
 
 class Tags(BaseBlog):
   tagname = models.CharField('Tag Name', max_length=150, null=True, blank=True)
@@ -15,7 +15,7 @@ class Blog(BaseBlog):
   title = models.CharField('Title', max_length=150, null=False, blank=False)
   blogDetail = models.TextField('Blog', null=False, blank=False)
   category = models.CharField('Category', max_length=200, null=False, blank=False)
-  tags = models.ManyToManyField(Tags)
+  tags = models.ManyToManyField(Tags, blank=False)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   
   def __str__(self):
