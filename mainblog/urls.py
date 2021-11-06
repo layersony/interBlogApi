d2fe017e2ca1ddfa8f9b8764624d86d4591f66d9
layersony <layersony@gmail.com>
2021-11-06 15:21:04 -0400

    blog tags views and urls add

--- mainblog/urls.py
+++ mainblog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('blogs', views.BlogView.as_view()), # Get All Blog
+  path('blogPost/', views.BlogPostView.as_view()), # Post A Blog
+  path('blogs/<int:pk>', views.BlogDetailView.as_view()), # For Specific blog
   path('auth/register', views.UserRegistrationView.as_view(), name='register'),
   path('auth/login', views.UserLoginView.as_view(), name='login'),
+  path('tags/', views.TagView.as_view(), name='tags')
 ]
--- mainblog/views.py
+++ mainblog/views.py
@@ -44,5 +44,105 @@ class UserLoginView(APIView):
       }
 
       return Response(response, status =status.HTTP_200_OK)
+ 
+class BlogView(APIView):
+  serializer_class = BlogSerializer
 
-# Create your views here.
+  def get(self, request, format=None):
+    allblogs = Blog.objects.all()
+    serializer = self.serializer_class(allblogs, many=True)
+    response = {
+      'success': True,
+      'status_code': status.HTTP_200_OK,
+      'message': 'Fetched Blogs',
+      'blogs': serializer.data
+    }
+    return Response(response, status=status.HTTP_200_OK)
+
+class BlogPostView(APIView):
+  serializer_class = BlogSerializer
+  permission_classes = (IsAuthenticated,)
+
+  def post(self, request, format=None):
+    serializers = self.serializer_class(data=request.data)
+    if serializers.is_valid():
+      serializers.save(author=request.user)
+      return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BlogDetailView(APIView):
+
+  serializer_class = BlogSerializer
+  permission_classes = (IsAuthenticated,)
+
+  def getblog(self, pk):
+    try:
+      return Blog.objects.get(pk=pk)
+    except: 
+      res = {
+        'message': f'Blog {pk} Not Found',
+        'status': status.HTTP_204_NO_CONTENT
+      }
+      return Response(res)
+
+  """
+  Get single blog with primary key
+  """
+  def get(self, request, pk, format=None):
+    blog = self.getblog(pk)
+    try:
+      serializers = self.serializer_class(blog)
+      return Response(serializers.data)
+    except:
+      res = {
+        'message': f'Blog {pk} Not Found',
+        'status': status.HTTP_204_NO_CONTENT
+      }
+      return Response(res)
+
+
+  """
+  Update single blog with primary key
+  """
+  def put(self, request, pk, format=None):
+    blog = self.getblog(pk)
+    serializers = self.serializer_class(blog, request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data)
+    else:
+      return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+  """
+  delete single blog with primary key
+  """
+  def delete(self, request, pk, format=None):
+    blog = self.getblog(pk)
+    blog.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+class TagView(APIView):
+  serializer_class = TagSerializer
+  permission_classes = (IsAuthenticated,)
+
+  def get(self, request, format=None):
+    try:
+      alltags = Tags.objects.all()
+      serialTag = TagSerializer(alltags, many=True)
+      response = {
+        'success': True,
+        'status_code': status.HTTP_200_OK,
+        'message': 'Tags',
+        'Tags': serialTag.data
+      }
+      return Response(response, status=status.HTTP_200_OK)
+    except:
+      
+      return Response('Url is Via Post Append Slash at the End')
+
+  def post(self, request, format=None):
+    serializers = self.serializer_class(data=request.data)
+    if serializers.is_valid():
+      serializers.save()
+      return Response(serializers.data, status=status.HTTP_201_CREATED)
+    return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
